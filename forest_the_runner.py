d1f1e086dd4f5dc2ff30b733f29ab799b63469e7
kseniiaguk <43299642+kseniiaguk@users.noreply.github.com>
2018-12-07 19:49:40 +0300

    UPDATE
    smth new

---
+++ forest_the_runner.py
@@ -0,0 +1,112 @@
+import pygame
+from levels import l1, l2, l3, l4, l5, l6, l7, l8, l9, Platform #li (1<=i<=9) - это список платформ - уровень, из файла levels.py. Platform - класс платформ
+from players import Player #Player - класс игроков
+
+pygame.init()
+win = pygame.display.set_mode((1280, 720))
+
+pygame.display.set_caption("Guk & Khodosov prod.")
+
+bg = pygame.image.load('win.jpg')
+
+
+logo = pygame.image.load('logo.jpg')
+
+player2 = ('find1.png')
+player1 = ('geo.png')
+
+pl1win = pygame.image.load('1win.jpg')
+pl2win = pygame.image.load('2win.jpg')
+
+limit = ('lim.png') # Картинка для платформы
+x1 = 5
+y1 = 655
+up1 = False #Переменная, отвечающаяя на вопрос, находится ли игрок1 в прыжке
+
+x2 = 5
+y2 = 655
+up2 = False #Переменная, отвечающаяя на вопрос, находится ли игрок2 в прыжке
+
+#След. две переменные запихиваем в класс Player (соотв-но они теперь обладают всеми свойствами класса)
+pl2 = Player(x2, y2, player2)
+pl1 = Player(x1, y1, player1)
+
+left1 = left2 = right1 = right2 = False # переменные, отвечающие, движется ли игрок вправо/влево
+
+# тут должен быть лого
+
+# нужно отрисовывать окно, например, 5 секунд
+
+
+pygame.mixer.music.load('Pskov.mp3')
+pygame.mixer.music.play(-1)
+
+sprite_group = pygame.sprite.Group() #переменная (группа), в которой содержатся все элементы класса Sprite (и игроки, и платформы)
+# Добавление в группу каждого игрока:
+sprite_group.add(pl1)
+sprite_group.add(pl2)
+platforms = [] #Список всех платформ в конкретном уровне
+lev = l9 #l9 выбрано для примера. Саша, напиши код так, чтобы в зависимости от нажатой кнопки (1, 2, ... 9) выбирался нужный список (уровень - l1, l2,... l9)
+# Координаты платформ:
+x = 0 
+y = 0
+#Отображение уровня. Нельзя засовывать в цикл (ломается)
+for i, j in enumerate(lev): #движение по столбцам
+    for k, l in enumerate(j): #движение по строке
+        if l == '-':
+            pl = Platform(x, y) #переменная, отвечающая за расположение в координатах каждой новой платформы
+            sprite_group.add(pl) #добавление в группу новой платформы
+            platforms.append(pl) #добавление в список платформ новой платформы
+        x += 128 # переход к след. элементу строки, но теперь в координатах
+    y += 72 # переход к след. столбцу, но теперь в координатах
+    x = 0 # обнуление х, так как теперь просматривается следующая строка
+
+
+# Сделать таймеры, в том числе для лого
+
+timer = pygame.time.Clock()
+if 1 == 1: #здесь должен быть выбор уровня (Саня)
+    run1 = True
+    run2 = True
+    while run1 or run2:
+        pygame.time.delay(5)
+        win.blit(bg, (0,0))
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run1 = False
+                run2 = False
+            if event.type == pygame.KEYDOWN: #проверка, происходит ли НАжатие кнопки (в данный момент)
+                if event.key == pygame.K_LEFT:
+                    left1 = True
+                if event.key == pygame.K_RIGHT:
+                    right1 = True
+                if event.key == pygame.K_UP:
+                    up1 = True
+                if event.key == pygame.K_a:
+                    left2 = True
+                if event.key == pygame.K_d:
+                    right2 = True
+                if event.key == pygame.K_w:
+                    up2 = True
+            elif event.type == pygame.KEYUP: #проверка, произошло ли ОТжатие кнопки (в данный момент)
+                if event.key == pygame.K_LEFT:
+                    left1 = False
+                if event.key == pygame.K_RIGHT:
+                    right1 = False
+                if event.key == pygame.K_UP:
+                    up1 = False
+                if event.key == pygame.K_a:
+                    left2 = False
+                if event.key == pygame.K_d:
+                    right2 = False
+                if event.key == pygame.K_w:
+                    up2 = False
+        pl1.update(left1, right1, up1, platforms)#метод обноления в классе Player
+        pl2.update(left2, right2, up2, platforms)#^^^^^
+        sprite_group.draw(win)#отрисовка ВСЕХ элементов группы (и платформ, и игроков)
+        pygame.display.flip()
+        timer.tick(60)
+
+
+pygame.quit()
---
+++ players.py
@@ -0,0 +1,56 @@
+from pygame.sprite import Sprite, collide_rect
+import pygame
+
+speed = 8 
+JumpRange = 10
+
+gravity = 0.5
+
+class Player(Sprite): #класс Player наследует методы класса Sprite и является дочерним классом по отн. к нему
+    def __init__(self, x, y, player): #координаты и персонаж
+        Sprite.__init__(self)
+        self.image = pygame.image.load(player)#отображение героя
+        self.xvel = 0 #скорость по х
+        self.yvel = 0 #скорость по y
+        self.rect = self.image.get_rect() #выделение прямоугольной области для героя
+        self.rect.x = x # соответствующаяя координата, rect добавлен, для того чтобы игрок "воспринимался" программой как прямоугольник
+        self.rect.y = y
+        self.onGround = False #находится ли игрок на земле (на поверхности)
+
+    def update(self, left, right, up, platforms): #метод обновления положения игрока
+        if left: # движется ли влево
+            self.xvel = -speed #скорость "отрицательна", т.к. координаты уменьшаются
+        if right:
+            self.xvel = speed
+        if not(left or right):
+            self.xvel = 0
+
+        if up:
+            if self.onGround:
+                self.yvel = -JumpRange #если игрок на земле и собирается прыгнуть, то координаты по y вычитаются
+
+        if not self.onGround:
+            self.yvel += gravity #если игрок в воздухе и уже прыгнул, то он падает - координаты прибавляются по y
+
+        self.onGround = False
+        self.rect.x += self.xvel #перемещение по х
+        self.collide(self.xvel, 0, platforms)#выполнение проверки на столкновение с платформой по х
+        self.rect.y += self.yvel
+        self.collide(0, self.yvel, platforms)#выполнение проверки на столкновение с платформой по y
+
+    def collide(self, xvel, yvel, platforms): #метод проверяющий на столкновение игрока с платформой^^^
+        for pl in platforms: #проходимся по всему списку платформ
+            if collide_rect(self, pl):
+                if xvel > 0:
+                    self.rect.right = pl.rect.left # если произошло столкновение и скорость по х была +, то игрок столкнулся с левой стенкой платформы =>
+                    #=> выполняется спец. метод столкновения с лев. стенкой объекта
+                if xvel < 0:
+                    self.rect.left = pl.rect.right
+                if yvel > 0:
+                    self.rect.bottom = pl.rect.top # если произошло столкновение и скорость по y была +, то игрок столкнулся с верхней стенкой платформы =>
+                    #=> выполняется спец. метод столкновения с верх. стенкой объекта и =>
+                    self.onGround = True #=> игрок на земле (на поверхности)
+                    self.yvel = 0
+                if yvel < 0:
+                    self.rect.top = pl.rect.bottom
+                    self.yvel = 0
